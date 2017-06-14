b719ae3eaaae6b861e4e19185863a5a7c6c9e5a0
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2017-06-14 06:57:54 +0200

    Monthly reports.

--- lib/expenses/commander.rb
+++ lib/expenses/commander.rb
@@ -24,21 +24,18 @@ module Expenses
     def self.sum(data_file_path)
       data_lines = self.parse(data_file_path)
       data_lines.group_by { |line| line.date.cweek }.each do |week, lines|
-        all_types = lines.group_by(&:type).map do |(type, lines)|
-          "<magenta>#{type}</magenta> #{self.report_currencies(lines)}"
-        end
-
         date = lines.first.date
         monday = date - (date.wday == 0 ? 7 : date.wday - 1)
 
-        all_tags = lines.map(&:tag).uniq.map do |tag|
-          "<cyan>#{tag}</cyan> #{self.report_currencies(lines)}"
-        end
-
         puts "<green>Week #{monday.strftime('%d/%m')} – #{(monday + 7).strftime('%d/%m')}</green>:".colourise(bold: true)
-        puts "<bold>Spendings by tags:</bold> #{all_tags.join(' ')}".colourise
-        puts "<bold>Spendings by category:</bold> #{all_types.join(' ')}".colourise
-        puts "<bold>Total:</bold> #{self.report_in_all_currencies(lines)}\n\n".colourise
+        self.report(lines); puts
+
+        if monday.month != (monday + 7).month
+          expenses = data_lines.select { |expense| expense.date.year == monday.year && expense.date.month == monday.month }
+          puts "<blue>#{monday.strftime('%B')} #{monday.year}</blue>:".colourise(bold: true)
+          self.report(expenses)
+          self.report_locations(expenses); puts
+        end
       end
 
       puts "<bold>Total:</bold> #{self.report_in_all_currencies(data_lines)}".colourise
@@ -107,6 +104,28 @@ module Expenses
       File.open(data_file_path, 'w') do |file|
         file.puts(final_json)
       end
+    end
+
+    def self.report(expenses)
+      all_tags = expenses.map(&:tag).uniq.map do |tag|
+        "<cyan>#{tag}</cyan> #{self.report_currencies(expenses)}"
+      end
+
+      all_types = expenses.group_by(&:type).map do |type, expenses|
+        "<magenta>#{type}</magenta> #{self.report_currencies(expenses)}"
+      end
+
+      puts "<bold>Spendings by tags:</bold> #{all_tags.join(' ')}".colourise
+      puts "<bold>Spendings by category:</bold> #{all_types.join(' ')}".colourise
+      puts "<bold>Total:</bold> #{self.report_in_all_currencies(expenses)}".colourise
+    end
+
+    def self.report_locations(expenses)
+      all_locations = expenses.group_by(&:location).map do |location, expenses|
+        "<yellow>#{location}</yellow> #{self.report_currencies(expenses)}"
+      end
+
+      puts "<bold>Locations:</bold> #{all_locations.join(' ')}".colourise
     end
 
     def self.report_in_all_currencies(expenses)
